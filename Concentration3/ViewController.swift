6cc9258fb30b1664d8fa921fe106764f91180301
Ivanchiang0821 <ivanchiang.tw@gmail.com>
2018-05-07 17:02:23 +0800

    Initial Commit

---
+++ Concentration3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration3
+//
+//  Created by IvanChiang on 2018/5/7.
+//  Copyright © 2018 IvanChiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
